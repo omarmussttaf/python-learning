4ad06b3cb79837fcab8c1784eff6e643e6d8d155
Omar Mussttaf <omarjazat@gmail.com>
2026-09-21 16:44:34 +0300

    second week

---
+++ compare.py
@@ -0,0 +1,11 @@
+x = int(input("what is x?"))
+y = int(input("what is y?"))
+
+if x>y:#boolean exprestion
+    print("x is greater than y")
+
+if x<y:
+    print("y is greater than y")
+
+if x==y:
+    print("x is equal to y")
