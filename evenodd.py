2efe79a5b33aa579fbd7aa1759227d893b0f73a0
Omar Mussttaf <omarjazat@gmail.com>
2026-09-21 17:35:58 +0300

    even or odd by function

---
+++ evenodd.py
@@ -0,0 +1,18 @@
+def main():
+    x = int(input("what is x?"))
+
+    if is_even (x):
+        print("even")
+
+    else:
+        print("odd")
+
+def is_even(n):
+    return n%2 ==0
+   # if n % 2 == 0:
+   #     return True
+   # else:
+   #     return False
+
+main()
+    
